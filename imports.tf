f0953f6809bbd17aadbbf12994c1658c9ceb5a99
claudio Abud <clau@gmail.com>
2023-11-30 16:23:19 -0600

    initial commit

---
+++ imports.tf
@@ -0,0 +1,48 @@
+##################################################################################
+# IMPORTS
+##################################################################################
+
+# import {
+#   to = module.main.aws_vpc.this[0]
+#   id = "vpc-0769a1959c93674c7" #VPC
+# }
+
+# import {
+#   to = module.main.aws_subnet.public[0]
+#   id = "subnet-02e29b01b5aa542e6" #PublicSubnet1
+# }
+
+# import {
+#   to = module.main.aws_subnet.public[1]
+#   id = "subnet-05b8d90604fd54543" #PublicSubnet2
+# }
+
+# import {
+#   to = module.main.aws_internet_gateway.this[0]
+#   id = "igw-0b1c9c94f67776b38" #InternetGateway
+# }
+
+# import {
+#   to = module.main.aws_route.public_internet_gateway[0]
+#   id = "rtb-08a6c319cc41e73e5_0.0.0.0/0" #DefaultPublicRoute
+# }
+
+# import {
+#   to = module.main.aws_route_table.public[0]
+#   id = "rtb-08a6c319cc41e73e5" #PublicRouteTable
+# }
+
+# import {
+#   to = module.main.aws_route_table_association.public[0]
+#   id = "subnet-02e29b01b5aa542e6/rtb-08a6c319cc41e73e5" #PublicSubnet1/PublicRouteTable
+# }
+
+# import {
+#   to = module.main.aws_route_table_association.public[1]
+#   id = "subnet-05b8d90604fd54543/rtb-08a6c319cc41e73e5" #PublicSubnet2/PublicRouteTable
+# }
+
+# import {
+#   to = aws_security_group.ingress
+#   id = "sg-006115179302a68e5" #NoIngressSecurityGroup
+# }
