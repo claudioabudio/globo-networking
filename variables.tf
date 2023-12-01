2618707ff06951666b31fe8298d41ebada150bc6
claudio Abud <clau@gmail.com>
2023-12-01 11:11:26 -0600

    formatted files

--- variables.tf
+++ variables.tf
@@ -14,10 +14,10 @@ variable "prefix" {
   default     = "globo-dev"
 }
 
-variable environment {
-  type = string
+variable "environment" {
+  type        = string
   description = "(Optional) environment of all resources"
-  default = "development"
+  default     = "development"
 }
 
 variable "cidr_block" {
